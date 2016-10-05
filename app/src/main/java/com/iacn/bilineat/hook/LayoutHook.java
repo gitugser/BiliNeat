1e860778bb3c17b18acd4b2bf66acb3d708034e8
iAcn <iacnlyx@gmail.com>
2016-10-05 18:22:07 +0800

    布局 Hook 部分

---
+++ app/src/main/java/com/iacn/bilineat/hook/LayoutHook.java
@@ -0,0 +1,20 @@
+package com.iacn.bilineat.hook;
+
+import android.content.res.XResources;
+
+/**
+ * Created by iAcn on 2016/10/5
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class LayoutHook {
+
+    /**
+     * 布局Hook的具体实现方法
+     *
+     * @param res Xposed 的 XResources
+     */
+    public void doHook(XResources res) {
+
+    }
+}
