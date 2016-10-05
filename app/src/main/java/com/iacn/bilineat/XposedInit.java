7ea549ff88230a227c4cd5da89895f8a97472355
iAcn <iacnlyx@gmail.com>
2016-10-05 18:20:29 +0800

    主 Hook 入口类

---
+++ app/src/main/java/com/iacn/bilineat/XposedInit.java
@@ -0,0 +1,24 @@
+package com.iacn.bilineat;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by iAcn on 2016/10/5
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class XposedInit implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resParam) throws Throwable {
+
+    }
+}
