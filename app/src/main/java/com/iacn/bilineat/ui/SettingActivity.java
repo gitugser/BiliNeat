d7d2d2b5b0cccf4a9a8e0703d45463acc3c80142
iAcn <iacnlyx@gmail.com>
2016-10-05 19:00:17 +0800

    显示返回按钮

--- app/src/main/java/com/iacn/bilineat/ui/SettingActivity.java
+++ app/src/main/java/com/iacn/bilineat/ui/SettingActivity.java
@@ -2,6 +2,7 @@ package com.iacn.bilineat.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.iacn.bilineat.R;
 
@@ -12,5 +13,13 @@ public class SettingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StatusBarUtils.setColor(this, getResources().getColor(R.color.pink));
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        onBackPressed();
+        return true;
     }
 }
