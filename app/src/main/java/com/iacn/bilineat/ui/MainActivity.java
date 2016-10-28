7f26f8baf23d9414e1e0629e6f50865c4ea5e6d0
iAcn <iacnlyx@gmail.com>
2016-10-28 20:31:05 +0800

    第一次打开时提醒

--- app/src/main/java/com/iacn/bilineat/ui/MainActivity.java
+++ app/src/main/java/com/iacn/bilineat/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.iacn.bilineat.ui;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v13.app.FragmentPagerAdapter;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mPager;
     private BottomNavigationView mBottomBar;
+    private SharedPreferences mSharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +49,35 @@ public class MainActivity extends AppCompatActivity {
                 return pageList.size();
             }
         });
+
+        mSharePref = getSharedPreferences("setting", MODE_WORLD_READABLE);
+
+        if (mSharePref.getBoolean("show_explain", true)) showExplainDialog();
+    }
+
+    /**
+     * 显示说明提示框
+     */
+    private void showExplainDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("说明");
+        builder.setMessage("1.本应用仅供技术交流，免费无广告，请勿用于商业及非法用途，如产生法律纠纷与作者无关\n\n2.私自修改本应用、利用本应用牟利等行为，后果由修改/传播者承担\n\n3.使用本应用所造成的一切后果自负\n\n4.如果确定，即你已默认同意以上条款");
+
+        builder.setNegativeButton("不再提醒", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mSharePref.edit().putBoolean("show_explain", false).apply();
+            }
+        });
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
 }
