5ff60d74fb579cb2a4fc0ccaa6ea12713e332e1a
iAcn <iacnlyx@gmail.com>
2016-11-15 13:20:54 +0800

    Add version name to Constant.java

--- app/src/main/java/com/iacn/bilineat/Constant.java
+++ app/src/main/java/com/iacn/bilineat/Constant.java
@@ -6,6 +6,6 @@ package com.iacn.bilineat;
  */
 
 public class Constant {
-    public static final String[] supportVersions = {"4.27.0", "4.28.3", "4.29.0"};
+    public static final String[] supportVersions = {"4.27.0", "4.29.0", "4.29.1"};
     public static final String biliPackageName = "tv.danmaku.bili";
 }
