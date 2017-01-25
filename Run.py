03407df73912770d243e792ceca359d50b2925cb
iAcn <iacnlyx@gmail.com>
2017-01-25 14:29:30 +0800

    Run.py：进入提示

--- Run.py
+++ Run.py
@@ -12,7 +12,18 @@ def run():
     enter_bl_folder()
 
 
+def show_entrance():
+    print()
+    print('===================================')
+    print('‖                                 ‖')
+    print('‖    BiliNeat Adaptive Script     ‖')
+    print('‖           Author:iAcn           ‖')
+    print('‖                                 ‖')
+    print('===================================')
+
+
 if __name__ == '__main__':
     # 暂时用作工作目录
     os.chdir('C:/Users/Administrator/Desktop/iBiliPlayer-bili')
+    show_entrance()
     run()
