faefca5d478f7f93f7837b58dc3e12079bd54b23
iAcn <iacnlyx@gmail.com>
2017-03-27 13:17:45 +0800

    HookBean：建立

---
+++ app/src/main/java/me/iacn/bilineat/bean/HookBean.java
@@ -0,0 +1,19 @@
+package me.iacn.bilineat.bean;
+
+/**
+ * Created by iAcn on 2017/3/27
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class HookBean {
+
+    public String onlineHelper;
+    public String onlineCategoryGame;
+    public String onlineToolbarGame;
+    public String onlineFoundGame;
+    public String onlineGameCenter;
+
+    public String themeClass;
+    public String foundMall;
+    public String indexInnerClass;
+}
