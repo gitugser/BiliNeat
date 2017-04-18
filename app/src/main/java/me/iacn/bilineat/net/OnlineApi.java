cb3de6d02d27dd3caab85feee9affb083a23adc6
iAcn <iacnlyx@gmail.com>
2017-04-18 20:32:22 +0800

    在线获取 Hook 信息配置

---
+++ app/src/main/java/me/iacn/bilineat/net/OnlineApi.java
@@ -0,0 +1,63 @@
+package me.iacn.bilineat.net;
+
+import java.io.ByteArrayOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by iAcn on 2017/4/18
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class OnlineApi {
+
+    private static OnlineApi sLeanApi;
+    private static final String BASE_URL = "http://api.iacn.me/bilineat/";
+
+    public static OnlineApi getInstance() {
+        if (sLeanApi == null) {
+            synchronized (OnlineApi.class) {
+                if (sLeanApi == null) {
+                    sLeanApi = new OnlineApi();
+                }
+            }
+        }
+
+        return sLeanApi;
+    }
+
+    private OnlineApi() {
+    }
+
+    public String getNewestVersion() {
+        String url = BASE_URL + "version";
+        return getContentByHttp(url);
+    }
+
+    public String getAdapterFile(String bili) {
+        String url = BASE_URL + "adapterfile?bili=" + bili;
+        return getContentByHttp(url);
+
+    }
+
+    private String getContentByHttp(String url) {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            int len;
+
+            while ((len = conn.getInputStream().read()) != -1) {
+                stream.write(len);
+            }
+
+            return stream.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
