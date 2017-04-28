2dc017a538b8e4eb53136767f7c74dbd4063a57d
iAcn <iacnlyx@gmail.com>
2017-04-28 19:52:28 +0800

    HookHandler: 只从本地反序列化 HookBean

--- app/src/main/java/me/iacn/bilineat/hook/HookHandler.java
+++ app/src/main/java/me/iacn/bilineat/hook/HookHandler.java
@@ -2,14 +2,12 @@ package me.iacn.bilineat.hook;
 
 import android.content.Context;
 import android.content.res.XResources;
-import android.os.Bundle;
-import android.widget.Toast;
 
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 import me.iacn.bilineat.bean.HookBean;
-import me.iacn.bilineat.ui.StateFragment;
-import me.iacn.bilineat.util.HookBuilder;
 
 /**
  * Created by iAcn on 2017/3/27
@@ -18,8 +16,8 @@ import me.iacn.bilineat.util.HookBuilder;
 
 public class HookHandler {
 
-    public static void methodHook(ClassLoader loader, String currentVersion) {
-        HookBean bean = getHookBean(currentVersion);
+    public static void methodHook(ClassLoader loader, String configPath) {
+        HookBean bean = getHookBean(configPath);
 
         if (!bean.isEmpty()) {
             OnlineHook.doHook(loader, bean);
@@ -32,46 +30,21 @@ public class HookHandler {
         LayoutHook.doHook(res);
     }
 
-    /**
-     * 在哔哩哔哩启动时显示 Toast
-     */
-    public static void showStartToast(ClassLoader loader, final String message) {
-        HookBuilder.create(loader)
-                .setClass("tv.danmaku.bili.ui.splash.SplashActivity")
-                .setMethod("onCreate")
-                .setParamTypes(Bundle.class)
-                .setHookCallBack(new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                        Toast.makeText((Context) param.thisObject, message, Toast.LENGTH_LONG).show();
-                    }
-                }).hook();
-    }
+    private static HookBean getHookBean(String configPath) {
+        try {
+            File file = new File(configPath);
+            System.out.println("--------------->" + file.canRead());
 
-    public static void hookSelf(ClassLoader loader) {
-        Class<?> clazz = XposedHelpers.findClass(StateFragment.class.getName(), loader);
-        XposedHelpers.setStaticBooleanField(clazz, "sXposedRunning", true);
-    }
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(configPath));
+            HookBean bean = (HookBean) stream.readObject();
+            stream.close();
 
-    private static HookBean getHookBean(String currentVersion) {
-        HookBean bean = new HookBean();
+            System.out.println(bean);
+            return bean;
 
-        // 根据当前版本决定要Hook的类和方法名
-        switch (currentVersion) {
-            case "5.4.0":
-                bean.onlineHelper = "dvp";
-                bean.onlineCategoryGame = "h";
-                bean.onlineToolbarGame = "i";
-                bean.onlineUnicomSim = "j";
-                bean.onlineFoundGame = "k";
-                bean.onlineGameCenter = "l";
-
-                bean.themeClass = "exm";
-                bean.foundMall = "eiy";
-                bean.indexInnerClass = "ecp$5";
-                break;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new HookBean();
         }
-
-        return bean;
     }
 }
