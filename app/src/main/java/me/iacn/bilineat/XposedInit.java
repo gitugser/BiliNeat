1870a0db6daa4ab7cb85b694d60001ce6f60eb18
iAcn <iacnlyx@gmail.com>
2017-04-28 19:29:53 +0800

    XposedInit: Hook 并自动更新配置文件

--- app/src/main/java/me/iacn/bilineat/XposedInit.java
+++ app/src/main/java/me/iacn/bilineat/XposedInit.java
@@ -3,7 +3,10 @@ package me.iacn.bilineat;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,13 +17,16 @@ import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import me.iacn.bilineat.hook.HookHandler;
+import me.iacn.bilineat.net.UpdateConfigTask;
+import me.iacn.bilineat.ui.StateFragment;
 import me.iacn.bilineat.util.HookBuilder;
 
 import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.setStaticBooleanField;
 
 /**
  * Created by iAcn on 2016/10/5
@@ -38,13 +44,14 @@ public class XposedInit implements IXposedHookZygoteInit, IXposedHookLoadPackage
     }
 
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (BuildConfig.APPLICATION_ID.equals(loadPackageParam.packageName)) {
-            // Hook 自身
-            HookHandler.hookSelf(loadPackageParam.classLoader);
+    public void handleLoadPackage(final LoadPackageParam loadParam) throws Throwable {
+        if (BuildConfig.APPLICATION_ID.equals(loadParam.packageName)) {
+            // Hook 自己
+            Class<?> clazz = findClass(StateFragment.class.getName(), loadParam.classLoader);
+            setStaticBooleanField(clazz, "sXposedRunnloadParaming", true);
             return;
 
-        } else if (!Constant.biliPackageName.equals(loadPackageParam.packageName)) {
+        } else if (!Constant.biliPackageName.equals(loadParam.packageName)) {
             // Hook 哔哩哔哩
             return;
         }
@@ -52,33 +59,45 @@ public class XposedInit implements IXposedHookZygoteInit, IXposedHookLoadPackage
         Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
         Context context = (Context) callMethod(activityThread, "getSystemContext");
 
-        final String currentVersion = context.getPackageManager()
-                .getPackageInfo(Constant.biliPackageName, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT).versionName;
+        PackageManager manager = context.getPackageManager();
+        final String version = manager.getPackageInfo(Constant.biliPackageName, 0).versionName;
+        String filesDir = manager.getPackageInfo(BuildConfig.APPLICATION_ID, 0).applicationInfo
+                .dataDir + "/files";
 
-        // 判断插件是否支持当前哔哩哔哩版本
-        Set<String> set = new HashSet<>();
-        Collections.addAll(set, Constant.supportVersions);
+        final String configPath = isSupport(version, filesDir);
 
-        if (set.contains(currentVersion)) {
+        if (configPath != null) {
+            // 支持当前版本
             xSharedPref.reload();
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                HookHandler.methodHook(loadPackageParam.classLoader, currentVersion);
+                HookHandler.methodHook(loadParam.classLoader, configPath);
             } else {
                 // Lollipop 以下在 Application 初始化完成后再进行 Hook
-                HookBuilder.create(loadPackageParam.classLoader)
+                HookBuilder.create(loadParam.classLoader)
                         .setClass("tv.danmaku.bili.MainApplication")
                         .setMethod("onCreate")
                         .setHookCallBack(new XC_MethodHook() {
                             @Override
                             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                HookHandler.methodHook(loadPackageParam.classLoader, currentVersion);
+                                HookHandler.methodHook(loadParam.classLoader, configPath);
                             }
                         }).hook();
             }
         } else {
-            // 如果不支持当前哔哩哔哩版本,弹出 Toast 提示
-            HookHandler.showStartToast(loadPackageParam.classLoader, "哔哩净化暂不支持你的版本哦~");
+            // 不支持当前哔哩哔哩版本,弹出 Toast 提示并更新配置文件
+            HookBuilder.create(loadParam.classLoader)
+                    .setClass("tv.danmaku.bili.ui.splash.SplashActivity")
+                    .setMethod("onCreate")
+                    .setParamTypes(Bundle.class)
+                    .setHookCallBack(new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                            Context context = (Context) param.thisObject;
+                            Toast.makeText(context, "哔哩净化暂不支持你的版本哦~", Toast.LENGTH_LONG).show();
+                            new UpdateConfigTask(context).execute(version);
+                        }
+                    }).hook();
         }
     }
 
@@ -88,5 +107,26 @@ public class XposedInit implements IXposedHookZygoteInit, IXposedHookLoadPackage
 
         xSharedPref.reload();
         HookHandler.layoutHook(resParam.res);
+    }
+
+    /**
+     * 判断净化是否支持当前 B 站版本
+     *
+     * @return 配置文件路径
+     */
+    private String isSupport(String version, String filesDir) {
+        File file = new File(filesDir);
+        String[] listDir = file.list();
+
+        if (listDir != null) {
+            Set<String> set = new HashSet<>();
+            Collections.addAll(set, listDir);
+
+            if (set.contains(version)) {
+                return filesDir + "/" + version;
+            }
+        }
+
+        return null;
     }
 }
