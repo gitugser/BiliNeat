66611ade66452035bdeb7300a7f2ca3bc3e20807
iAcn <iacnlyx@gmail.com>
2017-05-28 19:25:21 +0800

    AdaptedBean: 建立

---
+++ app/src/main/java/me/iacn/bilineat/bean/AdaptedBean.java
@@ -0,0 +1,14 @@
+package me.iacn.bilineat.bean;
+
+import java.util.List;
+
+/**
+ * Created by iAcn on 2017/5/28
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class AdaptedBean {
+
+    public int code;
+    public List<String> adaptedVersion;
+}
